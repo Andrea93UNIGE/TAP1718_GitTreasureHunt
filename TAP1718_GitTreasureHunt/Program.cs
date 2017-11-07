75f1ee01580a0cc3e09a7431ff506a2e071e6b83
Luca Demetrio <luca.demetrio@outlook.it>
2017-11-07 10:01:55 +0100

    Implementata funzione voluta fortemente dalla maggioranza degli Stakeholder.
    ... se non conoscete questa parola, potete andare qui http://www.wordreference.com/enit/stakeholder

--- TAP1718_GitTreasureHunt/Program.cs
+++ TAP1718_GitTreasureHunt/Program.cs
@@ -7,10 +7,7 @@ namespace TAP1718_GitTreasureHunt
     {
         public static void Main(string[] args)
         {
-            var rand = new Random();
-            int meme_index = rand.Next(5);
-            var meme = @"meme" + (meme_index + 1).ToString() + ".jpg";
-            System.Diagnostics.Process.Start(meme);
+            System.Diagnostics.Process.Start("congrats.jpg");
         }
     }
 }
