7a7daeb42c66f54d78f2ef84e4fcb0bdc757e5f9
Luca Demetrio <luca.demetrio@outlook.it>
2017-11-07 10:55:04 +0100

    Creata constante di vitale importanza.
    Un esempio di programmazione molto utile.
    Leggete e usate.
    WAT JS

--- TAP1718_GitTreasureHunt/NothingHere.cs
+++ TAP1718_GitTreasureHunt/NothingHere.cs
@@ -3,8 +3,10 @@ namespace TAP1718_GitTreasureHunt
 {
     public class NothingHere
     {
+        public const string nannannannannannanBATMAN = "nannannannannannanBATMAN";
         public NothingHere()
         {
+            
         }
     }
 }
