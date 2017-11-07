033b937f46875c07a19ab44aeb68287790f3798d
Luca Demetrio <luca.demetrio@outlook.it>
2017-11-07 09:41:37 +0100

    Initial commit

---
+++ TAP1718_GitTreasureHunt/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TAP1718_GitTreasureHunt
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var rand = new Random();
+            int meme_index = rand.Next(5);
+            var meme = @"meme" + (meme_index + 1).ToString() + ".jpg";
+            System.Diagnostics.Process.Start(meme);
+        }
+    }
+}
