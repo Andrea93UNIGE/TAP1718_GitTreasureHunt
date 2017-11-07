8522d8555411704b99a3bbf82255edecf54f5507
Luca Demetrio <luca.demetrio@outlook.it>
2017-11-07 10:39:02 +0100

    La feature aggiunta è importantissima.
    Questa feat aiuterà l'utente a ricordare shortcut da tastiera.

--- TAP1718_GitTreasureHunt/Program.cs
+++ TAP1718_GitTreasureHunt/Program.cs
@@ -8,6 +8,11 @@ namespace TAP1718_GitTreasureHunt
         public static void Main(string[] args)
         {
             Console.WriteLine("Non capisco cosa sto scrivendo. Maybe glibberish");
+            Console.WriteLine("Non capisco cosa sto scrivendo. Maybe glibberish");
+            Console.WriteLine("Non capisco cosa sto scrivendo. Maybe glibberish");
+            Console.WriteLine("Non capisco cosa sto scrivendo. Maybe glibberish");
+            Console.WriteLine("Non capisco cosa sto scrivendo. Maybe glibberish");
+            Console.WriteLine("... si mi piace la shortcut del Ctrl + V");
         }
     }
 }
