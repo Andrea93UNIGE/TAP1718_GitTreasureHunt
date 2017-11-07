57e6337e6c9de49c8a2d47dbd2cc12c90479fa2e
Luca Demetrio <luca.demetrio@outlook.it>
2017-11-07 10:28:31 +0100

    ... forse non era così importante.
    B1_Best è stata rimossa.

--- TAP1718_GitTreasureHunt/B1_Best.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace TAP1718_GitTreasureHunt
-{
-    public static class B1_Best
-    {
-        public const string THE_DEFAULT_VALUE = "Simply the best class in the world.";
-    }
-}
--- TAP1718_GitTreasureHunt/Program.cs
+++ TAP1718_GitTreasureHunt/Program.cs
@@ -7,10 +7,7 @@ namespace TAP1718_GitTreasureHunt
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Indizio 2:");
-            Console.WriteLine("Cercate la commit in cui è stato rimosso File.cs.");
-            Console.WriteLine("... non è troppo difficile.");
-            Console.WriteLine(B1_Best.THE_DEFAULT_VALUE);
+            Console.WriteLine("B1_Best non era così bella.");
         }
     }
 }
